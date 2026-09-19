43c30c2143d10083ae93e0b9490fa21026893e97
shweta3407 <shweta3407t>
2026-09-19 22:40:51 +0530

    @media

---
+++ SHWETA JS/javScriptDemo.js
@@ -0,0 +1,201 @@
+
+
+
+// console.log("hello shweta");
+// const name="shweta";
+// const age=24;
+// const pi = 3.14;
+// let  year="ty";
+// const isFollow=true;
+
+// const student = {
+//     name: "shweta" ,
+//     age : 24,
+//     subject : "IT" ,
+//     year : "ty"
+// }
+
+// student.name="ankita";
+// student.age=33;
+
+// const name="payal";
+// name="ppppp";
+
+
+
+
+// const product = {
+//     name :  "Ball Pen",
+//     rating : 4, 
+//     isDeal : true ,
+//     offer:  5   ,
+//     price : 270
+
+// }
+
+
+
+// const profile ={
+//     name : "shweta" ,
+//     ifFollow : true ,
+//     post : 123,
+//     following : 654,
+//     followers : 4343,
+//     email : "shweta@email.com",
+//     bio : "student learning java script"
+// }
+
+ 
+
+
+/////////////consitional operators
+
+// let a = 5;
+// let b =  5;
+
+// if( a % b  == 0){
+//     console.log("even");
+// }else{
+//     console.log("odd")
+// }
+
+
+ 
+
+
+
+// const age=35;
+
+// const result = age <= 18  ? "teenager" : "adult " ;
+// console.log(result);
+ 
+
+// if(age < 18){
+//     console.log("teenager")
+// }else if(age <60){
+//     console.log("adult")
+// }else if(age >60){
+//     console.log("old");
+// }
+
+
+
+
+
+ 
+// let  marks = 33;
+// let score;
+// if( marks >=90 &&  marks <=100){
+//     score="A"
+// }else if (  marks >=70 &&  marks<=89){
+//    score="B"
+// }else if (  marks >=60 &&  marks<=69){
+//     score="C"
+// }else if (  marks >=50 &&  marks<=59){
+//     score="D"
+// }else if (  marks >=0 &&  marks<=49){
+    
+//     score="F"
+// }
+// console.log(score);
+
+
+/////////////////////////////loops and string
+
+
+// ////initialization / stoping condition / updation
+// for(let i=0 ; i<=5  ; i++){
+//     console.log(i);
+// }
+
+// let sum=0;
+// for(let i = 0 ; i <=100 ; i++){
+//     sum+=i;
+// }
+// console.log(sum);
+    
+
+// ///initializer
+// let i=0;
+// while(i<=5){///stoping condition
+//     console.log(i);
+//     i++;////update
+// }
+
+
+
+
+// let j=1;
+// do{
+//  console.log(j);
+// j++
+// }while(j <= 5);
+
+
+
+// //for of loop
+// let name="shweta";
+// for(let i of name){
+
+//     console.log(i);
+// }
+
+
+
+
+// //for in loop
+// let student={
+//     name : "shweta",
+//     age : 22 ,
+//     standard : "ty",
+//     experience : "fresher"
+// }
+// for(let key in student){
+
+//     console.log("key : "  , key  , " value : " , student[key] );
+// }
+
+
+
+
+// for(let i=1 ; i <=100 ; i++){
+//     if(i % 2 !== 0 ){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+// console.log("number  gussing game");
+// let  randomNumber= 5;
+// let userNumber=prompt(" guess the  number : ");
+
+// let count=0;
+// while( userNumber != randomNumber){
+//     if(userNumber <  randomNumber){
+//         userNumber=prompt("to small guess more big number");
+//     }else if( userNumber > randomNumber){
+//         userNumber =prompt("to big guess small number");
+//     } 
+
+// }
+// console.log("   congratulation you won  , correct guess is " , randomNumber)
+
+
+
+// ////leteral string
+//  let name="shweta";
+// console.log(name);
+// let output = 'my  \t name \n  is ${1+ 2+444}';
+// console.log(  output);
+
+
+let str="hello there my name is shweta i am learning javascript";
+let newStr=str.toUpperCase;
+
+console.log(str);
+console.log(newStr);
+
+
